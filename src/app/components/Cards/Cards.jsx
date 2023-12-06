28f835cff2f195c63352be723bb64251d2382653
leonardo mendoza <leo7mendoza7@gmail.com>
2023-12-06 16:40:54 -0300

    craeted cards

---
+++ src/app/components/Cards/Cards.jsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import dataCards from "./DataCards";
+import Image from "next/image";
+
+const Cards = () => {
+  return (
+    <div className=" p-4 max-w-6xl w-full mx-auto ">
+      <h1 className="text-3xl font-semibold pb-10 md:grid md:grid-cols-2">
+        Elige la tarjeta que mas
+        <span className="block bg-blueLight degradeBlue">
+          se adapte a tus necesidades
+        </span>
+      </h1>
+      <div>
+        {dataCards.map(({ id, image }) => (
+          <Image
+            key={id}
+            src={image}
+            width={400}
+            height={300}
+            alt={`image-${id}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
---
+++ src/app/components/Cards/DataCards.js
@@ -0,0 +1,16 @@
+const dataCards = [
+  {
+    id: 1,
+    image: "/assets/card-1.png",
+  },
+  {
+    id: 2,
+    image: "/assets/card-2.png",
+  },
+  {
+    id: 3,
+    image: "/assets/card-3.png",
+  },
+];
+
+export default dataCards;
