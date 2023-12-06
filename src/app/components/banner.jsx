72f98a0cb5d2b6648bbf9374c48f1a7b5bc2a028
leonardo mendoza <leo7mendoza7@gmail.com>
2023-12-06 15:11:13 -0300

    created banner

--- src/app/components/banner.jsx
+++ src/app/components/banner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="p-4 max-w-6xl">
+    <div className="p-4 max-w-6xl w-full mx-auto">
       <section>
         <div>
           <h1 className="text-4xl font-semibold">
