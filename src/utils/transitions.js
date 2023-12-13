c74547f16a53fcd0b7df32bf6c364d17435933c5
leonardo mendoza <leo7mendoza7@gmail.com>
2023-12-13 18:18:02 -0300

    delay removing

--- src/utils/transitions.js
+++ src/utils/transitions.js
@@ -7,21 +7,21 @@ const transitionFadeIn = () => {
       transition: {
         type: "tween",
         duration: 0.5,
-        delay: 0.5,
         ease: [0.25, 0.6, 0.3, 0.8],
       },
     },
     visible: {
       y: 0,
-      opacity: 1,
       x: 0,
+      opacity: 1,
       transition: {
         type: "tween",
         duration: 1.4,
-        delay: 0.5,
+
         ease: [0.25, 0.25, 0.25, 0.75],
       },
     },
   };
 };
+
 export default transitionFadeIn;
